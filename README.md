0f0886f3f65f559c9d1a0dc44a7076082a188177
perezfeli <feli.perezz.1302@gmail.com>
2025-04-04 18:34:45 -0300

    Subida de archivos
    Ingrese los archivos del punto 10, el src con la funcion que utiliza el punto 10, el readme con las instrucciones de instalacion y el txt con los requirements

---
+++ README.md
@@ -0,0 +1,23 @@
+como instale la carpeta de github:
+primero descargue el zip de la carpeta desde el repositorio Practica2.
+
+instalar dependecias y luego ejecutar:
+
+para este programa utilizaremos notebook, si queremos ejecutar este programa desde un jupyter notebook se necesita realizar la instalacion.
+para esto ingresaremos la siguiente linea en nuestra terminal:
+
+pip install -r requirements.txt
+
+luego de esto se deberia haber realizado la descarga
+
+///////////////////////
+
+ejecucion del programa:
+luego de realizar todos los pasos anteriores ingresamos en nuestra terminal la siguiente linea:
+
+jupyter notebook
+
+con este comando ingresamos a un servidor de jupiter, ahora abriremos nuestro archivo .ipynb que esta en la carpeta de notebooks.
+primero ejecutamos la primera celda de codigo para tener acceso a las funciones, para luego poder ejecutar la segunda celda de codigo y poner a funcionar nuestro programa.
+
+para cerrar este servidor debemos poner ctrl+c en nuestra terminal y confirmarlo poniendo y
---
+++ src/funciones.py
@@ -0,0 +1,39 @@
+def ingresardatos(): 
+    rounds = [
+    { 
+    'Shadow': {'kills': 2, 'assists': 1, 'deaths': True},
+    'Blaze': {'kills': 1, 'assists': 0, 'deaths': False},
+    'Viper': {'kills': 1, 'assists': 2, 'deaths': True},
+    'Frost': {'kills': 0, 'assists': 1, 'deaths': False},
+    'Reaper': {'kills': 1, 'assists': 1, 'deaths': False}
+    },
+    { 
+    'Shadow': {'kills': 0, 'assists': 2, 'deaths': False},
+    'Blaze': {'kills': 2, 'assists': 0, 'deaths': True},
+    'Viper': {'kills': 1, 'assists': 1, 'deaths': False},
+    'Frost': {'kills': 2, 'assists': 1, 'deaths': True},
+    'Reaper': {'kills': 0, 'assists': 1, 'deaths': False}
+    },  
+    {
+    'Shadow': {'kills': 1, 'assists': 0, 'deaths': False},
+    'Blaze': {'kills': 2, 'assists': 2, 'deaths': True},
+    'Viper': {'kills': 1, 'assists': 1, 'deaths': True},
+    'Frost': {'kills': 0, 'assists': 1, 'deaths': False},
+    'Reaper': {'kills': 1, 'assists': 1, 'deaths': False}
+    },
+    {
+    'Shadow': {'kills': 2, 'assists': 1, 'deaths': False},
+    'Blaze': {'kills': 1, 'assists': 0, 'deaths': True},
+    'Viper': {'kills': 0, 'assists': 2, 'deaths': False},
+    'Frost': {'kills': 1, 'assists': 1, 'deaths': True},
+    'Reaper': {'kills': 1, 'assists': 1, 'deaths': False}
+    },
+    {
+    'Shadow': {'kills': 1, 'assists': 2, 'deaths': True},
+    'Blaze': {'kills': 0, 'assists': 1, 'deaths': False},
+    'Viper': {'kills': 2, 'assists': 0, 'deaths': True},
+    'Frost': {'kills': 1, 'assists': 1, 'deaths': False},
+    'Reaper': {'kills': 1, 'assists': 1, 'deaths': True}
+    }
+    ]
+    return rounds
